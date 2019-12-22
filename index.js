82ae0630bf2f1e404aaf0caf6d4d50212b73f1a9
Makhmudjon <maxmudinadullayev98@gmail.com>
2019-12-22 19:55:01 +0500

    electron index file modified to environment variables

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow, ipcRenderer } = require('electron');
 
 function createWindow() {
     let win = new BrowserWindow({ width: 800, height: 600 })
-    win.loadURL('http://localhost:3000')
+    win.loadURL(process.env.REACT_APP_ELECTRON_LOAD_URL)
 }
 
 app.on('ready', createWindow);
